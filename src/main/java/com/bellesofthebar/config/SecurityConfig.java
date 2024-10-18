c6e5991af95d84bab864706106c386eab297589e
Brighton Young <bright.young@outlook.com>
2024-10-18 13:57:06 -0400

    updating frontend

--- src/main/java/com/bellesofthebar/config/SecurityConfig.java
+++ src/main/java/com/bellesofthebar/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/", "/register", "/login").permitAll()
+                        .requestMatchers("/", "/register", "/login", "/actuator/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
